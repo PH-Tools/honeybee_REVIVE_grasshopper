b216637a58029904d6032ae856d365a387aa6023
PH Tools <phtools@bldgtyp.com>
2024-09-18 21:39:56 +0200

    update components

--- honeybee_revive_rhino/gh_compo_io/set_material_properties.py
+++ honeybee_revive_rhino/gh_compo_io/set_material_properties.py
@@ -4,16 +4,6 @@
 """GH-Component Interface: HB-REVIVE - Set Material or Construction REVIVE Properties."""
 
 try:
-    from typing import Any
-except ImportError:
-    pass  # IronPython 2.7
-
-try:
-    from honeybee_energy.material.opaque import EnergyMaterial
-except ImportError:
-    raise ImportError("\nFailed to import honeybee_energy")
-
-try:
     from honeybee_ph_rhino import gh_io
     from honeybee_ph_rhino.gh_compo_io import ghio_validators
 except ImportError as e:
@@ -23,6 +13,10 @@ try:
     from ph_units.unit_type import Unit
 except ImportError:
     raise ImportError("\nFailed to import ph_units")
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# -- Protocol Classes
 
 
 class __SupportsReviveAttributesProtocol__(object):
@@ -38,7 +32,31 @@ class __SupportsReviveAttributesProtocol__(object):
         raise NotImplementedError("This is a protocol class and should not be instantiated.")
 
 
-class GHCompo_SetMaterialOrConstructionProperties(object):
+class __SupportsRevivePropertiesProtocol__(object):
+    """Protocol class for type-checking."""
+
+    def __init__(self):
+        self.revive = __SupportsReviveAttributesProtocol__()
+
+    def __new__(cls):
+        raise NotImplementedError("This is a protocol class and should not be instantiated.")
+
+
+class __SupportsPropertiesProtocol__(object):
+    """Protocol class for type-checking."""
+
+    def __init__(self):
+        self.properties = __SupportsRevivePropertiesProtocol__()
+
+    def duplicate(self):
+        # type: () -> __SupportsPropertiesProtocol__
+        raise NotImplementedError("This is a protocol class and should not be instantiated.")
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+class GHCompo_SetMaterialProperties(object):
     kg_CO2_per_m2 = ghio_validators.UnitKG_M2("kg_CO2_per_m2", default=0.0)
     cost_per_m2 = ghio_validators.UnitCost_M2("kg_CO2_per_m2", default=0.0)
     labor_fraction = ghio_validators.FloatPercentage("labor_fraction", default=0.0)
@@ -46,7 +64,7 @@ class GHCompo_SetMaterialOrConstructionProperties(object):
     def __init__(
         self, _IGH, _hb_object, _kg_CO2_per_m2, _cost_per_m2, _labor_fraction, _lifetime_years, *args, **kwargs
     ):
-        # type: (gh_io.IGH, EnergyMaterial, float, float, float, int, list, dict) -> None
+        # type: (gh_io.IGH, __SupportsPropertiesProtocol__, float, float, float, int, list, dict) -> None
         self.IGH = _IGH
         self.hb_object = _hb_object
         self.kg_CO2_per_m2 = _kg_CO2_per_m2
@@ -55,11 +73,11 @@ class GHCompo_SetMaterialOrConstructionProperties(object):
         self.lifetime_years = _lifetime_years
 
     def run(self):
-        # type: () -> Any
+        # type: () -> __SupportsPropertiesProtocol__ | None
         if not self.hb_object:
             return None
         new_hb_obj = self.hb_object.duplicate()
-        new_mat_prop = getattr(new_hb_obj.properties, "revive")  # type: __SupportsReviveAttributesProtocol__
+        new_mat_prop = new_hb_obj.properties.revive
         new_mat_prop.kg_CO2_per_m2 = Unit(self.kg_CO2_per_m2, "KG/M2")
         new_mat_prop.cost_per_m2 = Unit(self.cost_per_m2, "COST/M2")
         new_mat_prop.labor_fraction = self.labor_fraction
