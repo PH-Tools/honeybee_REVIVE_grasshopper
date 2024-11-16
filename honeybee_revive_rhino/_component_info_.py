d86bdcfb5ea91a1fce3611c4a910aa4788164cff
PH Tools <phtools@bldgtyp.com>
2024-11-16 18:51:51 -0500

    Update
    - Update installer version
    - Update Test GH files

--- honeybee_revive_rhino/_component_info_.py
+++ honeybee_revive_rhino/_component_info_.py
@@ -5,7 +5,7 @@
 These are called when the component is instantiated within the Grasshopper canvas.
 """
 
-RELEASE_VERSION = "Honeybee-REVIVE v0.0.07"
+RELEASE_VERSION = "Honeybee-REVIVE v0.0.08"
 CATEGORY = "HB-REVIVE"
 SUB_CATEGORIES = {
     0: "00 | Utils",
